9cd27ad08942cdcafa4c599ec8df483738d27640
biniam <biniamdegu55@gmail.com>
2024-04-25 11:30:12 -0900

    updatin endpoints

--- frontend/src/components/ReduxSaga/Types/SongAPI.js
+++ frontend/src/components/ReduxSaga/Types/SongAPI.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const getSongAPI = async () =>
-  await axios.get("http://localhost:5500/getSongs");
+  await axios.get("https://music-player-final.onrender.com/getSongs");
 export const postSongAPI = async (sendFile) => {
   const data = await axios.post(
-    "http://localhost:5500/addSongs",
+    "https://music-player-final.onrender.com/addSongs",
     sendFile
   );
   // console.log(data)
@@ -12,13 +12,13 @@ export const postSongAPI = async (sendFile) => {
 };
 export const deleteSongAPI = async (id) => {
   const data = await axios.delete(
-    "http://localhost:5500/deleteSong/" + id
+    "https://music-player-final.onrender.com/deleteSong/" + id
   );
   return data;
 };
 export const updateSongAPI = async (id, artist) => {
   const data = axios.patch(
-    "http://localhost:5500/updateSongs/" + id,
+    "https://music-player-final.onrender.com/updateSongs/" + id,
     artist
   );
   return data;
